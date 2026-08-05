46c6cab90330e8d53e5bbd989f7b76582553e2a9
Suhad Rahman <suhadturan03@gmail.com>
2026-08-05 22:58:11 +0600

    Changes remaining at Lemonade shop
    Problem Link: https://www.codechef.com/practice/course/greedy-algorithms/INTGRA01/problems/TDQVH01?tab=statement

---
+++ TDQVH01.cpp
@@ -0,0 +1,35 @@
+bool canProvideChange(vector<int>& customerBills, int numberOfBills) {
+    
+    int cash = 0;
+    int fives = 0, tens = 0;
+    
+    for(int i = 0; i<numberOfBills; i++) {
+        if(customerBills[i] == 5) {
+            fives++;
+        }
+        else if(customerBills[i] == 10) {
+            tens++;
+            if(fives < 1) {
+                return false;
+            }
+            else{
+                fives--;
+            }
+        }
+        else if(customerBills[i] == 20) {
+            if(fives > 0 && tens > 0) {
+               fives--;
+               tens--;
+            }
+            else if(fives > 2) {
+                fives -= 3;
+            }
+            else {
+                return false;
+            }
+        }
+        
+    }
+    return true;
+}
+
