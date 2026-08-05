26157331adb715cb49ba20970a53fa15ba56733b
Suhad Rahman <suhadturan03@gmail.com>
2026-08-05 19:46:21 +0600

    Moneymaking
    Problem Link: https://www.codechef.com/START161D/problems/NUGGET

---
+++ NUGGET.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int X, Y;
+    
+    scanf("%d %d", &X, &Y);
+    
+    
+    int Total_Money=(X*5000+ Y*9800);
+    
+    printf("%d\n", Total_Money);
+    
+    return 0;
+	// your code goes here
+
+}
+
