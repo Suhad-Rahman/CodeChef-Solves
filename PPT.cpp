bbfcf49924402d011367f4714f0281a081d68780
Suhad Rahman <suhadturan03@gmail.com>
2026-08-05 22:55:17 +0600

    Presentation
    Problem Link: https://www.codechef.com/START250D/problems/PPT

---
+++ PPT.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve() {
+    
+    int n;
+    cin>>n;
+    
+    int slide = 600/30;
+    
+    cout<<slide - n<<"\n";
+}
+
+int32_t main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    
+    // int t;
+    // cin>>t;
+    
+    solve();
+
+return 0;
+}
